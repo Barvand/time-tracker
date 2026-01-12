d15393f72352103c9f802f90b0285a49fa873be1
B <128028346+Barvand@users.noreply.github.com>
2026-01-12 20:07:09 +0100

    time bug

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -44,7 +44,11 @@ export function mondayOfISOWeek(year: number, week: number) {
 
 export function formatForInputLocal(dateString: string) {
   const date = new Date(dateString);
-  const offset = date.getTimezoneOffset();
-  const local = new Date(date.getTime() - offset * 60 * 1000);
-  return local.toISOString().slice(0, 16);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${year}-${month}-${day}T${hours}:${minutes}`;
 }
